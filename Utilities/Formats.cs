54dc432b5b417764b3dc7e8edb638a33614e42e5
Agorist-Action <agorist_action@protonmail.com>
2020-12-24 22:17:59 -0600

    Remove Unused PriceFormat Class

--- Utilities/Formats.cs
+++ Utilities/Formats.cs
@@ -4,11 +4,6 @@ using System.Text;
 
 namespace Monero.Client.Utilities
 {
-    internal class PriceFormat
-    {
-        public readonly static string TwelveDecimalPlaces = "N12";  
-    }
-
     internal class DateFormat
     {
         public readonly static string DateTimeFormat = "yyyy-MM-dd HH:mm:ss";
