8630a0a3a0064169cbf73557fcf36af9ce42809d
Agorist-Action <agorist_action@protonmail.com>
2020-12-24 20:52:54 -0600

    Remove Empty Line Before ToString() Method

--- Wallet/POD/SubaddressIndex.cs
+++ Wallet/POD/SubaddressIndex.cs
@@ -8,7 +8,6 @@ namespace Monero.Client.Wallet.POD
         public uint Major { get; set; }
         [JsonPropertyName("minor")]
         public uint Minor { get; set; }
-
         public override string ToString()
         {
             return $"{Major} / {Minor}";
