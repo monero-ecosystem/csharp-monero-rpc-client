5c9396f37f8e8cb6ff7abd3c1e6cf72e5c466232
Agorist-Action <agorist_action@protonmail.com>
2021-01-01 11:27:02 -0600

    Add PriceUtilityTests

--- Utilities/PriceUtilities.cs
+++ Utilities/PriceUtilities.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
+[assembly: InternalsVisibleTo("Monero.Client.Tests")]
 namespace Monero.Client.Utilities
 {
+
     internal class PriceUtilities
     {
+        private const int LowestBase = 12;
+        private const decimal Piconero = 0.000000000001M;
+        private const decimal Nanonero = 0.000000001M;
+        private const decimal Micronero = 0.000001M;
+        private const decimal Millinero = 0.001M;
+        private const decimal Centinero = 0.01M;
+        private const decimal Decinero = 0.1M;
+        private const decimal Monero = 1M;
+        private const decimal Decanero = 10M;
+        private const decimal Hectonero = 100M;
+        private const decimal Kilonero = 1000M;
+        private const decimal Meganero = 1000000M;
+
         public static decimal PiconeroToMonero(ulong amount)
         {
             decimal piconero = amount;
@@ -15,10 +31,12 @@ namespace Monero.Client.Utilities
 
         public static ulong MoneroToPiconero(decimal amount)
         {
-            int decimals = GetDecimalPlaces(amount);
-            const int decimalPlaces = 12;
-            int decimalToAddOrRemove = decimalPlaces - decimals;
-            amount = amount * (decimal)Math.Pow(10, decimalToAddOrRemove);
+            if (amount < decimal.Zero)
+                throw new InvalidOperationException("Cannot have a negative amount of Monero");
+            int decimalPlaces = GetDecimalPlaces(amount);
+            if (decimalPlaces > LowestBase)
+                throw new InvalidOperationException($"Amount has more than {LowestBase} decimal places.");
+            amount = amount * (decimal)Math.Pow(10, LowestBase);
             return checked((ulong)amount);
         }
 
