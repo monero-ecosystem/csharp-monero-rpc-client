b8fc02c2dd79fa7b47d402b589559009473ff2c1
Ryan Williams <ryanmichaelwilliams@gmail.com>
2022-10-31 11:01:47 -0600

    UriBuilderTests

---
+++ src/Tests/MoneroClient.UnitTests/Network/UriBuilderTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using Monero.Client.Utilities;
+using Xunit;
+
+namespace MoneroClient.UnitTests.Network
+{
+    public class UriBuilderTests
+    {
+        [Theory]
+        [InlineData("monero.com", 8080, "rpc", "http://monero.com:8080/rpc")]
+        [InlineData("xmr.something.com", 1234, "rpc/json", "http://xmr.something.com:1234/rpc/json")]
+        public void UriBuilder_ValidParams_ReturnsCorrectUrl(string host, uint port, string endpoint, string expectedUrl)
+        {
+            var url = new Monero.Client.Network.UriBuilder(host, port, endpoint).Build();
+            Assert.Equal(expectedUrl, url.ToString());
+        }
+    }
+}
